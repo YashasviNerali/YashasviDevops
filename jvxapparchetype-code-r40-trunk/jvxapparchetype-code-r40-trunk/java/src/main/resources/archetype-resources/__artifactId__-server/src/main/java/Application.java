33848b2be16ea53c24fee5d9c7c6cbd17be427c3
YashasviNerali <48079498+YashasviNerali@users.noreply.github.com>
2019-03-20 15:14:18 +0530

    Add files via upload

---
+++ jvxapparchetype-code-r40-trunk/jvxapparchetype-code-r40-trunk/java/src/main/resources/archetype-resources/__artifactId__-server/src/main/java/Application.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import com.sibvisions.rad.server.GenericBean;
+
+/**
+ * The LCO for the application.
+ */
+public class Application extends GenericBean
+{
+
+}	// Application
